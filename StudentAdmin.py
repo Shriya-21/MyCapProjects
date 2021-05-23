0f205953cd16378c79caf959f703aae730c83a77
Shriya-21 <84094822+Shriya-21@users.noreply.github.com>
2021-05-24 00:07:52 +0530

    StudentAdmin.py
    A program that collects student data and writes it onto a CSV file

--- StudentAdmin.py
+++ StudentAdmin.py
@@ -4,17 +4,20 @@ def write_into_csv_file(info_list):
         writer=csv.writer(csv_file)
 
         if csv_file.tell()==0:
+           
             writer.writerow(["First Name","Last Name","Gender","DoB","Contact Number","E-Mail ID"])
 
         writer.writerow(info_list)
-    
-    
+                            
 if __name__=='__main__':
     
-    check = 'yes'
+    entry_check = 'yes'
     student_num = 0
+    list1=["yes","Yes","YES"]
+    list2=["yes","Yes","YES","no","No","NO"]
+    list3=["no","No","NO"]
 
-    while check == 'yes' or check == 'Yes' or check == 'YES':
+    while entry_check in list1:
 
         student_info = input ("\nEnter the student information in  the following format:\n[First Name, Last Name, Gender, DoB(DD/MM/YYYY), Contact Number, E-Mail ID]:\n")
         
@@ -23,18 +26,24 @@ if __name__=='__main__':
         print("\nEntered information is - \nFirst Name: {}\nLast Name: {}\nGender: {}\nDoB: {}\nContact Number: {}\nE-Mail ID: {}"
               .format(student_list[0],student_list[1],student_list[2],student_list[3],student_list[4],student_list[5]))
 
-        correct_check= str (input ("\nIs the information entered correct?(yes/no): "))
+        info_verify= str (input ("\nIs the information entered correct?(yes/no): "))
 
-        if correct_check == 'Yes' or correct_check == 'yes': 
+        if info_verify in list1: 
             student_num += 1
 
             write_into_csv_file(student_list)
 
-            check = str (input ("\nDo you want to enter the details of another student?(yes/no): "))
+            entry_check = str (input ("\nDo you want to enter the details of another student?(yes/no): "))
 
-            while check != 'yes' and check != 'no' and check != 'Yes' and check != 'No' and check != 'YES' and check != 'NO':
+            while entry_check not in list2:
                 print ("Invalid entry, try again")
-                check = str (input ("\nDo you want to enter the details of another student? (yes/no): "))
+                entry_check = str (input ("\nDo you want to enter the details of another student? (yes/no): "))
 
-        elif correct_check == 'No' or correct_check == 'no':
-            print("\nPlease re-enter the correct values!\n")
+        elif info_verify in list3:
+            print("\nPlease re-enter the correct values!")
+
+
+            
+                                
+                    
+
